473df404ad9fdc6937ddcce26c782ee5c8386206
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-01-30 01:35:49 -0300

    meu primeiro arquivo em js

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+console.log(`a velocidade do seu carro está em ${velocidade}km/h`)
+if (velocidade > 60) {
+    console.log(`acima da velocidade permitida, multado!`)
+}
+console.log('use sempre o cinto de segurança!')
