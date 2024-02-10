2c50bfd8f8241aa40aba8d9bdab1d34be0a07ef2
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-02-10 12:05:36 -0300

    aula 12

---
+++ aula012/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16
+if (idade < 16) {
+    console.log('não vota')
+} else if (idade < 18  || idade > 65) {
+    console.log('vota se quiser')
+} else if (idade >= 18) {
+    console.log('voto obrigatório')
+} 
