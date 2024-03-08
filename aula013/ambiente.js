b857fc2c9520baae53caa8bd65949e48e571edd4
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-03-08 12:02:50 -0300

    estruturas de repetições parte 1

---
+++ aula013/ambiente.js
@@ -0,0 +1,18 @@
+ //ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO: estrutura "while"
+ // ótima para repetiçóes em grande quantidade. 
+ 
+ var c = 1
+ while (c <= 500) {
+    console.log(`passo ${c}`)
+    c++ // mais 1
+ }
+ 
+ /* 
+  console.log('olá, Neo.')
+  console.log('olá, Neo.') 
+  console.log('olá, Neo.')
+  console.log('olá, Neo.')
+  console.log('olá, Neo.')
+
+          (...)  
+  */
---
+++ aula013/ambiente02.js
@@ -0,0 +1,8 @@
+//ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL: estrutura "do"
+
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+ } while (c <= 6)
+// essa estrutura tem a mesma funcionalidade que a "while", é apenas uma forma diferente de fazer a mesma tarefa.
