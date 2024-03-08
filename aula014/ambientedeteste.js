083cab0621672ad9b4bc80d9be90c4016980562e
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-03-08 12:36:22 -0300

    estrutura for

---
+++ aula014/ambientedeteste.js
@@ -0,0 +1,15 @@
+// ESTRUTURA "FOR":
+
+console.log('vai começar...')
+for(var c = 1; c <= 50; c++) {
+    console.log(c)
+} 
+console.log("FIM!")
+//início (variável) > teste lógico > incremento (c++)
+
+
+/* var c = 1 
+while (c <= 100) {
+    console.log(c)
+    c++
+} */
