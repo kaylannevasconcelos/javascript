5e8a6314cc25a7e0824ea753c8d6b3eec467150b
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-04-27 12:40:38 -0300

    exercicio de contagem

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,32 @@
-alert('olá')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { 
+        window.alert('[ERRO] faltam dados!')
+        res.innerHTML = `Impossível contar!`
+    } else {
+        res.innerHTML = 'contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('passo inválido! considerando PASSO 1')
+            p = 1
+        }
+        /* contagem CRESCENTE */
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${ c } \u{1F449}`
+            }
+        /* contagem REGRESSIVA */
+        } else {
+            for( let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
