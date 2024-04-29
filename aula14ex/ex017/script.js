01ea30eba41c005426364e257d465050ed7f2117
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-04-29 02:15:31 -0300

    criando tabuada com método WHILE

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('por favor, digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' /* serve para limpar antes do próximo resultado */
+        while (c <=10) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item) /* adicionando a equação dentro da área da tabuada */
+            c++
+        }
+    }
+}
