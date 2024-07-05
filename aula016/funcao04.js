cb34ae5e3ce268cdd394bb75f3714b19016d21f6
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-07-05 10:50:52 -0300

    recursividade

--- aula016/funcao04.js
+++ aula016/funcao04.js
@@ -7,3 +7,5 @@ function fatorial(n) {
 }
 
 console.log(fatorial(6))
+
+// fatorial da maneira tradicional.
--- aula016/funcao05.js
+++ aula016/funcao05.js
@@ -1,3 +1,11 @@
 // RECURSIVIDADE
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(5))
 
-//deu por hoje
+// isso é fatorial da maneira recursiva, onde o fatorial faz uma chamada dele mesmo.
