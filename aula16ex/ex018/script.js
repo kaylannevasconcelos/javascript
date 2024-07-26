fcc0b19d68fbbbdda5e4e8e9e3e20ed59886c0f0
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2024-07-26 16:19:47 -0300

    ...

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,1 +1,28 @@
-alert('olá')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('input#flista')
+let res = document.querySelector("div#res")
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, 1) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
