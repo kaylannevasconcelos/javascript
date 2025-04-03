e77b598979fcfd0ab2fdb93c68dc70b1f9193bb9
Kaylanne Vasconcelos <151457751+kaylannevasconcelos@users.noreply.github.com>
2025-04-03 17:41:16 -0300

    update

--- aula016/funcao01.js
+++ aula016/funcao01.js
@@ -1,4 +1,4 @@
-function parimpar(n) {
+function parimpar(n) {   /* A VARIÁVEL "PARIMPAR" FOI DECLARADA DENTRO DA FUNÇÃO */
     if (n%2 == 0) {
         return "par!"
     } else {
@@ -6,7 +6,7 @@ function parimpar(n) {
     }
 }
 
-let res = parimpar(293)
+let res = parimpar(293)   /* NESSE CASO A VARIÁVEL "RES" REPRESENTA "PARIMPAR" SOMENTE QUANDO ELA TEM O VALOR 293 */
 console.log(res)
 
 /* console.log(parimpar(293)) */
